7c067076d158af278c2f1dd3c8e053edf8a59877
gongjunhan <735900315@qq.com>
2026-03-19 17:41:34 +0800

    first

---
+++ src/test/java/com/gonjunhan/helloserver/HelloServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonjunhan.helloserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
